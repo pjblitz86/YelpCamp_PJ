141d715cb1bd7f820706c24461423b689877aad6
pjblitz86 <paulius.juknis@gmail.com>
2022-10-10 12:39:26 +0300

    more error handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -42,6 +43,8 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('invalid campground data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -84,7 +87,13 @@ app.delete(
   })
 );
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('page not found', 404));
+});
+
 app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'sum ting wong' } = err;
+  res.status(statusCode).send(message);
   res.send('sum ting wong!');
 });
 
