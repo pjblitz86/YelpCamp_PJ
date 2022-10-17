81896efe32c55104fa720e3bde667e1645665221
pjblitz86 <paulius.juknis@gmail.com>
2022-10-17 14:57:23 +0300

    flash success message partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,6 +65,7 @@ router.put(
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
+    req.flash('success', 'Successfully updated the campground!');
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -74,6 +75,7 @@ router.delete(
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted the campground!');
     res.redirect('/campgrounds');
   })
 );
