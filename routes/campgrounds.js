e890c90956600d472a99dcc6f46c95ba0d556cfa
pjblitz86 <paulius.juknis@gmail.com>
2022-10-19 09:56:23 +0300

    isLoggedIn middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const { campgroundSchema } = require('../schemas');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const Review = require('../models/review');
+const { isLoggedIn } = require('../middleware');
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -24,12 +24,13 @@ router.get(
   })
 );
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new');
 });
 
 router.post(
   '/',
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
@@ -55,6 +56,7 @@ router.get(
 
 router.get(
   '/:id/edit',
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
@@ -67,6 +69,7 @@ router.get(
 
 router.put(
   '/:id',
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -80,6 +83,7 @@ router.put(
 
 router.delete(
   '/:id',
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
