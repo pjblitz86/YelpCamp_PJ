49ae8c1b4f39190aa0235be266bc4f9924f41bec
pjblitz86 <paulius.juknis@gmail.com>
2022-10-20 12:34:48 +0300

    storing uploaded cloudinary img ref to mongo and display

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,8 +11,13 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
+  campground.image = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename
+  }));
   campground.author = req.user._id;
   await campground.save();
+  console.log(campground);
   req.flash('success', 'Successfully made a new campground!');
   res.redirect(`campgrounds/${campground._id}`);
 };
@@ -22,8 +27,8 @@ module.exports.showCampground = async (req, res) => {
     .populate({
       path: 'reviews',
       populate: {
-        path: 'author',
-      },
+        path: 'author'
+      }
     })
     .populate('author');
   if (!campground) {
@@ -46,7 +51,7 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
+    ...req.body.campground
   });
   req.flash('success', 'Successfully updated the campground!');
   res.redirect(`/campgrounds/${campground._id}`);
--- models/campground.js
+++ models/campground.js
@@ -4,28 +4,33 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
   title: String,
-  image: String,
+  image: [
+    {
+      url: String,
+      filename: String
+    }
+  ],
   price: Number,
   description: String,
   location: String,
   author: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'User'
   },
   reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
-    },
-  ],
+      ref: 'Review'
+    }
+  ]
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
     await Review.deleteMany({
       _id: {
-        $in: doc.reviews,
-      },
+        $in: doc.reviews
+      }
     });
   }
 });
