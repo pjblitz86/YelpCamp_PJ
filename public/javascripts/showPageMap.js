129207d4f56c7e551e4639d7cb9622bcbc303ed2
pjblitz86 <paulius.juknis@gmail.com>
2022-10-21 14:23:55 +0300

    connecting map to current campground location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8
 });
+
+new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
